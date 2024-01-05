e9efb93d0dff410b477c33e32b88fdee0ef2a7ce
nehanth <ccrecruiter462.vr@gmail.com>
2024-01-05 14:18:51 +0530

    edited

--- Classes_Objects.py
+++ Classes_Objects.py
@@ -2,9 +2,12 @@ class Animal:
   def __init__(self,name,age):
     self.name=name
     self.age=age
+
+
   def sound(self,s):
     self.sound=s
     print(self.sound)
 
 dog = Animal("nero","12")
+print("Hello",dog.name,"You are",dog.age,"years old.")
 dog.sound("bow bow")
